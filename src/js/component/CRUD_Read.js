5069fa2f47cff40f8a53048513410837f2c475c3
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-08-15 13:28:51 +0000

    test CRUD

--- src/js/component/CRUD_Read.js
+++ src/js/component/CRUD_Read.js
@@ -8,13 +8,8 @@ const CRUD_Read = () => {
 
   const fetchData = async () => {
     try {
-      // Make GET request to API
       const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/alesanchezr');
-      
-      // Await and parse response as JSON
       const json = await response.json();
-      
-      // Set state with data from API
       setData(json);
       
     } catch (error) {
@@ -28,9 +23,10 @@ const CRUD_Read = () => {
 
   return (
     <div className="mx-auto my-auto p-3" style={{width:"280px", height:"800px"}}>
-      {/* Display data from API */}
       <h2 className="m-3"> GET (READ)</h2>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+
+      {data}
     </div>
   );
 }
